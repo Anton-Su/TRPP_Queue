f353e6d6a7923a9ee2151ce658665fde186daf7e
Antua <suleykin2005@mail.ru>
2025-03-16 19:36:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script. Antuan was here. Zurab Shottovish was not.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
