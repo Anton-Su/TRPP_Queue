084d68f9302e718fc6897d55a454aba3c8570324
Antua <suleykin2005@mail.ru>
2025-03-27 04:08:13 +0300

    failed_attempt_to_import

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="TRPP_Queue",
     version="0.1",
     packages=find_packages(),
+    py_modules=["main", "schedule", "validation", "deletion"],
     install_requires=[
     'dotenv>=0.9.9',
     'python-dotenv>=1.0.1',
@@ -12,6 +13,11 @@ setup(
     'requests>=2.32.3',
     'icalendar>=6.1.2',
     ],
+    package_data={
+        # Указываем все файлы .db в текущей директории
+        "": ["*.db", "*.py"],  # Включаем все .py и .db файлы
+    },
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'kiezunago=main:main',  # точку входа для бота
